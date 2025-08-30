9e495c9b5cbc86a966ecee5ab49b0c6661321048
felicia <feliciawillnas@gmail.com>
2025-08-30 22:57:31 +0200

    general layout information page

--- src/pages/Information.jsx
+++ src/pages/Information.jsx
@@ -1,9 +1,40 @@
-import React from "react";
+import styled from "styled-components";
 
 export function Information() {
   return (
     <>
-      <div>Information</div>
+      <Main>
+        <WhiteDiv></WhiteDiv>
+        <PinkDiv></PinkDiv>
+        <GrayDiv></GrayDiv>
+      </Main>
     </>
   );
 }
+
+const Main = styled.div`
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+`;
+
+const WhiteDiv = styled.div`
+  background-color: white;
+  height: 150px;
+  transform: rotate(1deg);
+  z-index: 10;
+`;
+
+const PinkDiv = styled.div`
+  background-color: pink;
+  height: 150px;
+  transform: rotate(-1deg);
+  z-index: 50;
+`;
+
+const GrayDiv = styled.div`
+  background-color: gray;
+  height: 150px;
+  transform: rotate(1deg);
+  z-index: 100;
+`;
