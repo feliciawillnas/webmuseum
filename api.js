006f9ea7832bd930546a2f393bf835cf851e26fb
felicia <feliciawillnas@gmail.com>
2025-09-05 00:14:12 +0200

    fetching 3 pages at a time

--- api.js
+++ api.js
@@ -4,8 +4,20 @@ const api = "https://api.artic.edu/api/v1/artworks";
 
 export async function fetchArtworks() {
   try {
-    const response = await axios.get(api);
-    return response.data.data;
+    // Previous solution
+    // const response = await axios.get(api);
+    // return response.data.data;
+
+    // Array of request results
+    const requests = [];
+
+    // Fetch first 3 pages
+    for (let i = 1; i < 4; i++) {
+      requests.push(axios.get(`${api}?page=${i + 1}&limit=20`));
+    }
+
+    const responses = await Promise.all(requests);
+    return responses.flatMap((response) => response.data.data);
   } catch (error) {
     console.error("Error fetching artworks:", error);
     return [];
