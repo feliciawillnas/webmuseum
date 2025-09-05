c373fb028c130479d533ce5bccc072d31e8757d5
felicia <feliciawillnas@gmail.com>
2025-09-05 18:48:01 +0200

    backup data for api

--- App.jsx
+++ App.jsx
@@ -1,8 +1,8 @@
 import { useEffect, useState } from "react";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
-import { fetchArtworks } from "./api";
 import { Header } from "./src/components/Header";
+import backupArtworks from "./src/data/artworks/backupArtworks.json";
 import { Gallery } from "./src/pages/Gallery";
 import { Home } from "./src/pages/Home";
 import { Information } from "./src/pages/Information";
@@ -29,22 +29,28 @@ function App() {
   const [artworks, setArtworks] = useState([]);
 
   // Fetch artworks from api
-  async function loadFallbackArtworks() {
-    const res = await fetch("./src/data/allArtworks.jsonl");
-    const text = await res.text();
-    return text
-      .trim()
-      .split("\n")
-      .map((line) => JSON.parse(line));
-  }
+  // async function loadFallbackArtworks() {
+  //   const res = await fetch("./src/data/allArtworks.jsonl");
+  //   const text = await res.text();
+  //   return text
+  //     .trim()
+  //     .split("\n")
+  //     .map((line) => JSON.parse(line));
+  // }
 
   useEffect(() => {
     async function loadData() {
       // Call fetchArtworks (api.js) store in data
-      const data = await fetchArtworks();
+      // const data = await fetchArtworks();
+      const data = [];
 
-      // Filter public domain artworks and store
-      setArtworks(data.filter((art) => art.is_public_domain));
+      if (!data.length) {
+        // Filter public domain artworks and store
+        setArtworks(backupArtworks.filter((art) => art.is_public_domain));
+      } else {
+        // Filter public domain artworks and store
+        setArtworks(data.filter((art) => art.is_public_domain));
+      }
     }
     loadData();
   }, []);
