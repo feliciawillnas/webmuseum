cead9c6ea13701b5d6b689e18cfb861232867b93
felicia <feliciawillnas@gmail.com>
2025-09-08 17:29:35 +0200

    home page design

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,34 +1,39 @@
 import styled from "styled-components";
-import image0 from "../assets/homepage_images/gianni_dova.png";
+import collage_five from "../assets/homepage_images/collage_five.jpg";
+import collage_four from "../assets/homepage_images/collage_four.jpg";
+import collage_one from "../assets/homepage_images/collage_one.jpg";
+import collage_seven from "../assets/homepage_images/collage_seven.jpg";
+import collage_six from "../assets/homepage_images/collage_six.jpg";
+import collage_three from "../assets/homepage_images/collage_three.jpg";
+import collage_two from "../assets/homepage_images/collage_two.jpg";
 
-export function Home({ artworks }) {
+export function Home() {
   return (
     <>
-      <Main>
-        <EmptyDiv>
-          {artworks.map((artwork) => (
-            <img key={artwork.id} src={artwork.imageUrl} alt={artwork.title} />
-          ))}
-        </EmptyDiv>
-        <BackgroundStyling></BackgroundStyling>
-      </Main>
+      <Collage>
+        <img src={collage_one} alt="" />
+        <img src={collage_two} alt="" />
+        <img src={collage_three} alt="" />
+        <img src={collage_four} alt="" />
+        <img src={collage_five} alt="" />
+        <img src={collage_six} alt="" />
+        <img src={collage_seven} alt="" />
+      </Collage>
     </>
   );
 }
 
-const Main = styled.div`
+const Collage = styled.div`
+  width: 100vw;
   display: flex;
-`;
+  position: absolute;
+  bottom: 0;
+  flex-direction: row;
+  align-items: baseline;
 
-const EmptyDiv = styled.div`
-  width: 50vw;
-`;
-
-const BackgroundStyling = styled.div`
-  background-image: url(${image0});
-  background-size: cover;
-  background-position: center;
-  background-repeat: no-repeat;
-  height: 100vh;
-  width: 50vw;
+  img {
+    width: calc(100% / 7);
+    height: auto;
+    object-fit: cover;
+  }
 `;
